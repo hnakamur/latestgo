524be784ce3d4cc47977436058e0b0f00e7930cc
Hiroaki Nakamura <hnakamur@gmail.com>
2018-12-05 05:27:47 +0900

    Add webapp

---
+++ cmd/latestgo-webapp/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/hnakamur/latestgo"
+)
+
+func main() {
+	listenAddr := flag.String("listen", ":8080", "listen address in host:port form")
+	queryTimeout := flag.Duration("query-timeout", 5*time.Second, "version query timeout")
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), *queryTimeout)
+		defer cancel()
+
+		w.Header().Set("Content-Type", "text/plain")
+
+		ver, err := latestgo.Version(ctx)
+		if err != nil {
+			log.Printf("failed to get latest go version; %s", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError)
+			return
+		}
+
+		_, err = w.Write([]byte(ver))
+		if err != nil {
+			log.Printf("failed to write latest go version; %s", err)
+			return
+		}
+	})
+
+	srv := http.Server{
+		Addr: *listenAddr,
+	}
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigC := make(chan os.Signal, 1)
+		signal.Notify(sigC, os.Interrupt, syscall.SIGTERM)
+		<-sigC
+
+		// We received an interrupt or SIGTERM signal, shut down.
+		if err := srv.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		// Error starting or closing listener:
+		log.Printf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
+}
