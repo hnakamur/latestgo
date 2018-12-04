ac3bc4b65c5b80fda3592565c0bbdc4f524e13f8
Hiroaki Nakamura <hnakamur@gmail.com>
2018-12-05 05:29:54 +0900

    Get version without go prefix

--- client.go
+++ client.go
@@ -5,9 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+const versionPrefix = "go"
 
 // Version returns the latest stable go version.
 func Version(ctx context.Context) (string, error) {
@@ -38,5 +41,9 @@ func Version(ctx context.Context) (string, error) {
 		return "", errors.New("id attribute not found in div.toggleVisible")
 	}
 
-	return ver, nil
+	if !strings.HasPrefix(ver, versionPrefix) {
+		return "", fmt.Errorf("id attribute div.toggleVisible does not start with %s, ver=%s", versionPrefix, ver)
+	}
+
+	return ver[len(versionPrefix):], nil
 }
